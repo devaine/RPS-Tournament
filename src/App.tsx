4067176c91d1db7e2d0b9f14a4298c12f4a817da
JK-le-dev <jkcondor13@gmail.com>
2024-12-17 17:48:48 -0600

    feat(identification): added identification portion
    Also, I learned how to center a div.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Signup from "./pages/signup";
+import Signup from "./pages/Signup";
+import Identification from "./pages/Identification";
 
+// Testing the prototype components
+// TODO: Add router that navigages between pages but keeps app as SPA
 function App() {
   return (
     <div>
       <Signup />
+      <Identification />
     </div>
   );
 }
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Signup() {
   return (
-    <div className="flex min-h-screen flex-col items-center">
+    <div className="flex h-screen flex-col items-center justify-center">
       <h1>signup</h1>
       <div className="flex flex-col">
         <Input title="Name" />
