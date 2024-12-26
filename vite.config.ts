cc496161f9d4baf177390bcba588593a991aa064
devaine <devaine1203@gmail.com>
2024-12-26 14:56:23 -0600

    removed node_modules from future commits, added a bit of into into README, and configured vite config to go to src/config rather than root

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite"
-import 'dotenv/config'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: ['./src/config/.env']})
 
 export default defineConfig({
 	server: {
-		port: Number(process.env.VITE_PORT) // Converted into a number
+		//port: Number(process.env.VITE_PORT) // Converted into a number
+		port: Number(process.env.VITE_PORT)
 	}
 })
