a50c98eebd97dc8b6905740fec2ae8684ca134ff
JK-le-dev <jkcondor13@gmail.com>
2025-02-09 16:09:16 -0600

    chore(image): changed from public to assets
    Photos were originally gonna be in public folder but are now instead in
    assets folder.

--- src/app/routes/auth/profile.tsx
+++ src/app/routes/auth/profile.tsx
@@ -3,14 +3,14 @@ import { Title } from "@/components/ui/text";
 import Button from "@/components/ui/button";
 import { AuthLayout } from "@/components/layouts/auth-layout";
 import Picture from "@/components/ui/picture";
+import player from "@/assets/img/players/player1.png"
 
 const Profile = () => {
-  const img = "/img/icons/player1.png";
 
   return (
     <AuthLayout>
       <Title text="Do you want to use this picture?" />
-      <Picture img={process.env.PUBLIC_URL + img} />
+      <Picture src={player} />
       <div className="flex gap-4">
         <Button text="Yes" link="/app"></Button>
         <Button text="No" link="/register"></Button>
--- src/config/icons.ts
+++
@@ -1,7 +0,0 @@
-export const icons = {
-  player: {
-    1: "/img/players/player1.png",
-    2: "/img/players/player2.png",
-  },
-  moves: {},
-};
