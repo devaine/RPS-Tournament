aa08d174f78accda29e59959de0238c9424933ee
Lance <lancefinke@icloud.com>
2025-02-09 16:16:09 -0600

    closing branch

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+// import React from "react";
+// import Signup from "./routes/Signup";
+// import Identification from "./routes/Identification";
+// import VisualDescription from "./routes/VisualDescription";
+// import Locate from "./routes/Locate";
+// import Waiting from "./routes/Waiting";
+// import RockPaperScissors from "./routes/RockPaperScissors";
+// import Decision from "./routes/Decision";
+
+import { AppRouter } from './router'
+
+export const App = () => {
+	return(
+		
+	)
+}
