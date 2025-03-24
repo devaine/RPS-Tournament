93609d8c23d576e01f5f33c89484bb3406f4bca9
JK-le-dev <jkcondor13@gmail.com>
2025-03-24 16:16:10 -0500

    chore(form): changed to Formik component+navigate
    Changed that and made it navigate to next page.

--- apps/frontend/src/app/routes/landing.tsx
+++ apps/frontend/src/app/routes/landing.tsx
@@ -3,8 +3,9 @@ import { Title } from "@/components/ui/text";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { RegisterLayout } from "@/components/layouts/register-layout";
-import { useFormik } from "formik";
+import { Formik } from "formik";
 import * as Yup from "yup";
+import { useNavigate } from "react-router";
 
 interface FormValues {
   name: string;
@@ -20,19 +21,9 @@ const Landing = () => {
   const validationSchema = Yup.object().shape({
     name: Yup.string().required(),
     id: Yup.string().length(7).required(),
-  })
-
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      id: "",
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-      console.log(values);
-    },
-    validationSchema: validationSchema,
   });
+
+  const navigate = useNavigate();
 
   return (
     <RegisterLayout>
@@ -43,27 +34,43 @@ const Landing = () => {
         <Title text="TOURNAMENT" />
       </div>
 
-      <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
-        <Input
-          id="name"
-          type="text"
-          label="First and Last Name"
-          onChange={formik.handleChange}
-          value={formik.values.name}
-          placeholder={placeHolderValues.name}
-          maxLength={30}
-        />
-        <Input
-          id="id"
-          type="number"
-          label="Student ID"
-          onChange={formik.handleChange}
-          value={formik.values.id}
-          placeholder={placeHolderValues.id}
-          maxLength={7}
-        />
-        <Button type="submit" text="Submit" link="/register"></Button>
-      </form>
+      <Formik
+        initialValues={{ name: "", id: "" }}
+        onSubmit={(values) => {
+          alert(JSON.stringify(values, null, 2));
+          navigate("/register");
+        }}
+        validationSchema={validationSchema}
+      >
+        {({
+          values,
+          handleChange,
+          handleSubmit,
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <Input
+              id="name"
+              type="text"
+              label="First and Last Name"
+              onChange={handleChange}
+              value={values.name}
+              placeholder={placeHolderValues.name}
+              maxLength={30}
+            />
+            <Input
+              id="id"
+              type="number"
+              label="Student ID"
+              onChange={handleChange}
+              value={values.id}
+              placeholder={placeHolderValues.id}
+              maxLength={7}
+            />
+            <Button type="submit" text="Submit" link="/register"></Button>
+          </form>
+        )}
+      </Formik>
     </RegisterLayout>
   );
 };
