816823fdb724af2823ed0c75fa7c8f27c9af5101
devaine <devaine1203@gmail.com>
2025-04-07 13:39:45 -0500

    merge: fixed stuff

--- apps/api/src/game.ts
+++ apps/api/src/game.ts
@@ -4,14 +4,12 @@ import { io } from "./index";
 export function gameManager(socket: Socket) {
   // FOR ADMIN PAGE
   // Fetch all socket ids
-  socket.on("startRound", async (callback) => {
-    const getSockets = await io.in("contestant_room").fetchSockets();
-    const listSockets: string[] = [];
-
 	let readyCount = 0
 
 	// FOR ADMIN PAGE
 	// Fetch all socket ids 
+	//
+	// NOTE: Fetches sockets and move sthem to "game_room"
 	socket.on("startRound", async (callback) => {
 		const getSockets = await io.in("contestant_room").fetchSockets()
 		const listSockets: string[] = []
@@ -42,40 +40,56 @@ export function gameManager(socket: Socket) {
     // io.to(String(player2)).socketsJoin("game_room")
     // io.to(String(player2)).socketsLeave("contestant_room")
 
-    const check = await io.in("game_room").fetchSockets();
-    const list = check.map(function (data) {
-      console.log(data.id);
-      return data.id;
-    });
-
-    callback(list);
   });
 
+
+
+	// NOTE: Removes Players
   socket.on("removePlayer", async (callback) => {
-    const getSockets = await io.in("game_room").fetchSockets();
-    const listSockets: string[] = [];
+   // const getSockets = await io.in("game_room").fetchSockets();
+   // const listSockets: string[] = [];
 
-    function randomNumber(min: number, max: number) {
-      return Math.floor(Math.random() * (max - min) + min);
-    }
+   // function randomNumber(min: number, max: number) {
+   //   return Math.floor(Math.random() * (max - min) + min);
+   // }
 
-    // Sends all socket ids into a array
-    for (const socket of getSockets) {
-      listSockets.push(socket.id);
-    }
+   // // Sends all socket ids into a array
+   // for (const socket of getSockets) {
+   //   listSockets.push(socket.id);
+   // }
 
-    const player = listSockets[randomNumber(0, getSockets.length)];
-    io.to(String(player)).socketsLeave("game_room");
-    io.to(String(player)).socketsJoin("contestant_room");
+   // const player = listSockets[randomNumber(0, getSockets.length)];
+   // io.to(String(player)).socketsLeave("game_room");
+   // io.to(String(player)).socketsJoin("contestant_room");
 
-    const check = await io.in("game_room").fetchSockets();
-    const list = check.map(function (data) {
-      console.log(data.id);
-      return data.id;
-    });
+   // const check = await io.in("game_room").fetchSockets();
+   // const list = check.map(function (data) {
+   //   console.log(data.id);
+   //   return data.id;
+   // });
 
-    callback(list);
+   // callback(list);
   });
+
+	socket.on("playerReady", async (callback) => {
+		const getSockets = await io.in("game_room").fetchSockets()
+		const listSockets: string[] = []
+	
+		for(const socket of getSockets) {
+			listSockets.push(socket.id)
+		}
+
+		console.log(io.listeners("playerReady").length)
+
+
+		if(io.listeners("playerReady").length === 2) {
+			console.log('KJFEJAFOHADOFASFJAFJSF')
+			callback("Play")
+		}
+	}
+)}
+
+
 }
 
 function decideWinner(choice1: string, choice2: string) {
