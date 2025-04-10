c1143b1d5e6e11f1f43d442c29b98daf829aaa0f
devaine <devaine1203@gmail.com>
2025-04-10 12:20:47 -0500

    forgot to pull from the latest tag

--- apps/frontend/src/app/index.tsx
+++ apps/frontend/src/app/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Router from "./router";
+import { LandingProvider } from "@/features/context/landing-context";
+import { DecisionProvider } from "@/features/context/decision-context";
+import { GameProvider } from "@/features/context/game-context";
 
 const App = () => {
   return (
     <div>
-      <Router />
+		<LandingProvider>
+			<DecisionProvider>
+				<GameProvider>
+					<Router />
+				</GameProvider>
+			</DecisionProvider>
+		</LandingProvider>
     </div>
   );
 };
