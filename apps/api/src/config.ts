385a62edb67b7a9664ff2c2349c0a8d0d8c8ac19
devaine <devaine1203@gmail.com>
2025-04-10 02:41:58 -0500

    deploy: testing again..

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -1,6 +1,6 @@
 // SERVER VARIABLES
 export const PORT = 3001;
-export const URL = "http://localhost:3001";
+export const URL = "https://rps-dev.devdoes.work";
 // export const URL = "http://10.162.167.86"; // For LSC testing
 
 export type LandingScreen = "Register" | "Game Started";
